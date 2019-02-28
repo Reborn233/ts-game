c858a976381617b371633d2f2ed834acf3362301
Reborn233 <499289113@qq.com>
2019-02-28 16:29:44 +0800

    git pages

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ exports.htmlPlugin = (options = {}) => {
       chunks: [name],
       favicon: 'src/favicon.ico',
       title: name,
-      url: options.dev ? '/assets' : '.'
+      url: options.dev ? '/assets' : '/ts-game'
     };
     arrHtml.push(new HtmlWebpackPlugin(config));
   });
