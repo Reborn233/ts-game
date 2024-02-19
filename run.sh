3a890013f4c15f7074b2b9621b6333ec0704bdfe
沈冬明 <shendm1@yonyou.com>
2024-02-19 18:27:45 +0800

    1

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ git config user.name "reborn233"
 git config user.email "499289113@qq.com"
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
-git push --force --quiet "https://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@github.com/Reborn233/ts-game.git" master:gh-pages
+git push --force --quiet "https://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@github.com/Reborn233/ts-game.git" master:gh-pages
